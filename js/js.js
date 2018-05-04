0cccb92211199ca56c9a9e5ce48f38b90276f901
xScottishKrissx <chrisdunne66@gmail.com>
2018-05-04 18:23:20 +0100

    Day 4 - Afternoon
    Ditched the old navigation bar and started working on my own. it's not a
    replica but it does fit the theme of the website using the same colours
    and fonts.

--- js/js.js
+++ js/js.js
@@ -1,5 +1,43 @@
 $(".x").on("click", function() {
 
   $(this).closest(".nav").toggleClass("open");
+  $(".search").toggleClass("move-search");
+});
+
+$(".close-menu").on("click", function() {
+  $(this).closest(".nav").toggleClass("open");
+  $(".search").toggleClass("move-search");
+});
+
+$(".glyphicon-search").on("click", function(){
+
+  $(".glyphicon-search").toggleClass("change-icon");
+  $(".search-box").toggleClass("expand-search");
+})
+
+/*
+jQuery(document).ready(function($){
+
+  $('.live-search-list li').each(function(){
+    $(this).attr('data-search-term', $(this).text().toLowerCase());
+  });
+
+  $('.live-search-box').on('keyup', function(){
+
+  var searchTerm = $(this).val().toLowerCase();
+
+      $('.live-search-list li').each(function(){
+
+          if ($(this).filter('[data-search-term *= ' + searchTerm + ']').length > 0 || searchTerm.length < 1) {
+              $(this).show();
+          } else {
+              $(this).hide();
+          }
+
+      });
+
+
+  });
 
 });
+*/
