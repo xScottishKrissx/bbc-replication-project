bb6c6d422e87102d675ac5885ed3f807b582054d
xScottishKrissx <chrisdunne66@gmail.com>
2018-05-09 12:54:23 +0100

    Day 7 - Morning
    Added JavaScript Todys Date to Header
    Cleaned Up Dropdown Transitions when adjusting window width
    Cleaned Up and Styled Footer.

--- js/js.js
+++ js/js.js
@@ -1,39 +1,112 @@
-$(".x").on("click", function() {
+$(document).ready( function(){
 
-  $(this).closest(".nav").toggleClass("open");
-  $(".search").toggleClass("move-search");
-});
 
-$(".close-menu").on("click", function() {
-  $(this).closest(".nav").toggleClass("open");
-  $(".search").toggleClass("move-search");
-});
+  $(".x").on("click", function() {
 
-$(".glyphicon-search").on("click", function(){
+    $(this).closest(".nav").toggleClass("open");
+    $(".search").toggleClass("move-search");
+  });
 
-  $(".glyphicon-search").toggleClass("change-icon");
-  $(".search-box").toggleClass("expand-search");
-})
+  $(".close-menu").on("click", function() {
+    $(this).closest(".nav").toggleClass("open");
+    $(".search").toggleClass("move-search");
+  });
 
-$(".oi-magnifying-glass").on("click", function(){
-  $(".mobile-search").toggleClass("toggle-mobile-search");
-})
+  $(".glyphicon-search").on("click", function(){
+    $(".glyphicon-search").toggleClass("change-icon");
+    $(".search-box").toggleClass("expand-search");
+  })
 
-$(".search-box").on('keyup', function(){
-  //console.log("Show Message");
-  //console.log(this.value.length);
+  $(".oi-magnifying-glass").on("click", function(){
+    $(".mobile-search").toggleClass("toggle-mobile-search");
+  })
 
-  var inputLength = this.value.length;
 
-  if (inputLength > 2) {
-    //console.log("ShowMessage");
-    $(".search-message").css("display","block");
+  //Show Live Search Message as you type
+  $(".search-box").on('keyup', function(){
+    //console.log("Show Message");
+    //console.log(this.value.length);
+    var winWidthOnKeyUp = $(window).width();
+    var inputLength = this.value.length;
+
+    //console.log(winWidth);
+
+    if (inputLength > 2 && winWidthOnKeyUp > 768) {
+      //console.log("ShowMessage");
+      $(".search-message").css("display","block");
+    }
+    else {
+      //console.log("hide message");
+      $(".search-message").css("display","none");
+    }
+  })
+
+  //Hide live search message when clicking off message and empty form
+  $(".weather-bar, .nav, .row-container").on("click",function(){
+    destroyLiveSearch();
+  })
+
+  //Hide Live Search message when window re-sizes
+  $(window).resize(function(){
+    var winWidthOnResize = $(window).width();
+    //console.log(winWidthOnResize);
+
+    if (winWidthOnResize < 768) {
+        destroyLiveSearch();
+    }
+
+    //Show & Hide Mobile Search if window resizes.
+    if (winWidthOnResize > 768) {
+      //$(".mobile-search").toggleClass("toggle-mobile-search");
+      $(".mobile-search").css("display", "none");
+    }else {
+      $(".mobile-search").css("display", "table");
+    }
+
+  })
+
+//Pointless Get Todays Date Task
+  var today = new Date();
+
+  var dd = today.getDate();
+  var mm = today.getMonth() + 1;
+  var yyyy = today.getFullYear();
+
+  if (dd<10) {
+    //dd = '0' + dd;
   }
-  else {
-    //console.log("hide message");
-    $(".search-message").css("display","none");
+
+  if (mm < 10) {
+    //mm = '0' + mm;
   }
+
+  function getDayName(dateStr, locale){
+    var date2 = new Date(dateStr);
+    return date2.toLocaleDateString(locale, {weekday: 'long'});
+  }
+
+  function getMonthName(monthStr, locale){
+    var monthName = new Date (dateStr);
+    return monthName.toLocaleDateString(locale, {month:'long'});
+  }
+
+  var dateStr = today;
+  var day = getDayName(dateStr, "EN-US");
+  var month = getMonthName(dateStr, "EN-US");
+  //console.log(day);
+  //console.log(month);
+
+  var today = day + ',' + ' ' + dd + ' '+ month;
+  document.getElementById("todaysDate").innerHTML = today;
+  //console.log(today);
 })
+
+function destroyLiveSearch(){
+  $(".search-message").css("display","none");
+  $(".search-box").val("");
+}
+
+
 
 
 /*
